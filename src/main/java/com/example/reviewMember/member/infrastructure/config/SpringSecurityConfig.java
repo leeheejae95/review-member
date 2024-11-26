2982649f83bce8f521ce175b5b26ca6175ca6bd6
leeheejae <leeheejae55@naver.com>
2024-11-26 09:16:44 +0900

    시큐리티 커밋

--- src/main/java/com/example/reviewMember/member/infrastructure/config/SpringSecurityConfig.java
+++ src/main/java/com/example/reviewMember/member/infrastructure/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.reviewMember.member.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +11,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
         http
                 .csrf(AbstractHttpConfigurer::disable); // HTTP 요청에 대한 보안 규칙 정의
         http
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers("/member").permitAll() // url 허용
-                                .anyRequest().authenticated() // 그 외 모든 요청은 인증 사용
+                                .requestMatchers("/member/login", "/member").anonymous() // url 허용
+                                .requestMatchers("/member/**").hasRole("REGISTERED")
+                                .anyRequest().permitAll() // 그 외 모든 요청은 인증 사용
                 );
         return http.build();
     }
---
+++ src/main/java/com/example/reviewMember/member/presentation/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.reviewMember.member.presentation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
